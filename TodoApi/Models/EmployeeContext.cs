59d4b4fe099a05bf957b3e7d64ebd51e0e165cc5
sraddy <sraddy@its.jnj.com>
2022-12-07 15:10:43 +0530

    Add project files.

---
+++ TodoApi/Models/EmployeeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TodoApi.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; } = null!;
+    }
+}
